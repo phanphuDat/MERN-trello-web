bd8916d3945b013f1aeaf4577941825dfd723e8a
phudat <phongdat.20022000@gmail.com>
2024-09-04 21:43:47 +0700

    import mui

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,18 +4,26 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react', 'react-hooks', 'react-refresh'],
   rules: {
+    // react
     'react-refresh/only-export-components': 'warn',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 0,
     'react/display-name': 0,
-
+    // mui
+    "no-restricted-imports": [
+      "error",
+      {
+        "patterns": ["@mui/*/*/*"]
+      }
+    ],
+    //common
     'no-console': 1,
     'no-lonely-if': 1,
     'no-unused-vars': 1,
@@ -33,6 +41,6 @@ module.exports = {
     'keyword-spacing': 1,
     'comma-dangle': 1,
     'comma-spacing': 1,
-    'arrow-spacing': 1,
-  },
-};
+    'arrow-spacing': 1
+  }
+}
